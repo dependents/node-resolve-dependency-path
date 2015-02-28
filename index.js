38e5321dcd323e1e3ad7a5b14eda79e16bae20bd
Jim Cummins <jcummins@fisdap.net>
2015-02-28 01:35:39 -0600

    Dependency paths with multiple periods should resolve.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var path = require('path');
+require('string.prototype.endswith');
 
 /**
  * Resolve a dependency's path
@@ -24,6 +25,12 @@ module.exports = function(dep, filename, directory) {
 
   if (!depExt) {
     filepath += fileExt;
+  } else {
+    // If a dependency starts with a period AND it doesn't already end
+    // in .js AND doesn't use a custom plugin, add .js back to path.
+    if (fileExt === '.js' && !dep.endsWith('.js') && dep.indexOf('!') < 0) {
+      filepath += fileExt;
+    }
   }
 
   return filepath;
