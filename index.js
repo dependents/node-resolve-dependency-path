6bad609f2cd9f5e740a78b8c2c05007903ed6ca8
Joel Kemp <mrjoelkemp@gmail.com>
2015-03-02 16:08:51 -0500

    Refactor

--- index.js
+++ index.js
@@ -12,34 +12,57 @@ module.exports = function(dep, filename, directory) {
   if (!filename) { throw new Error('filename not given'); }
   if (!directory) { throw new Error('directory not given'); }
 
-  var depExt = path.extname(dep);
-  var fileExt = path.extname(filename);
-  var filepath;
+  var filepath = getDependencyPath(dep, filename, directory);
+  var ext = getDependencyExtension(dep, filename);
 
-  if (isRelative(dep)) {
-    filepath = path.resolve(path.dirname(filename), dep);
-  } else {
-    filepath = path.resolve(directory, dep);
-  }
-
-  if (!depExt) {
-    filepath += fileExt;
-  } else {
-    // If a dependency starts with a period AND it doesn't already end
-    // in .js AND doesn't use a custom plugin, add .js back to path.
-    if (fileExt === '.js' && depExt !== '.js' && dep.indexOf('!') < 0) {
-      filepath += fileExt;
-    } else {
-      // If using a SystemJS style plugin
-      if (depExt.indexOf('!') > -1) {
-        filepath += depExt.substring(0, depExt.indexOf('!'));
-      }
-    }
-  }
-
-  return filepath;
+  return filepath + ext;
 }
 
+/**
+ * @param  {String}  dep
+ * @return {Boolean}
+ */
 function isRelative(dep) {
   return dep.indexOf('..') === 0 || dep.indexOf('.') === 0;
+}
+
+/**
+ * @param  {String} dep
+ * @param  {String} filename
+ * @param  {String} directory
+ * @return {String} Absolute path for the dependency
+ */
+function getDependencyPath(dep, filename, directory) {
+  if (isRelative(dep)) {
+    return path.resolve(path.dirname(filename), dep);
+  }
+
+  return path.resolve(directory, dep);
+}
+
+/**
+ * @param  {String} dep
+ * @param  {String} filename
+ * @return {String} The determined extension for the dependency (or empty if already supplied)
+ */
+function getDependencyExtension(dep, filename) {
+  var depExt = path.extname(dep);
+  var fileExt = path.extname(filename);
+
+  if (!depExt) {
+    return fileExt;
+  }
+
+  // If a dependency starts with a period AND it doesn't already end
+  // in .js AND doesn't use a custom plugin, add .js back to path
+  if (fileExt === '.js' && depExt !== '.js' && dep.indexOf('!') < 0) {
+    return fileExt;
+  }
+
+  // If using a SystemJS style plugin
+  if (depExt.indexOf('!') > -1) {
+    return depExt.substring(0, depExt.indexOf('!'));
+  }
+
+  return '';
 }
