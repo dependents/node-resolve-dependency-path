1a781a6938a974f4894e660bfdaddba5641bb286
Joel Kemp <mrjoelkemp@gmail.com>
2018-12-21 10:23:49 -0500

    Breaking: support an object form of arguments
    Refs https://github.com/dependents/node-filing-cabinet/issues/71

--- test/test.js
+++ test/test.js
@@ -4,7 +4,11 @@ var path = require('path');
 
 describe('resolve-dependency-path', function() {
   it('resolves with absolute paths', function() {
-    var resolved = resolvePath('./bar', __dirname + '/foo.js', __dirname);
+    var resolved = resolvePath({
+      dependency: './bar',
+      filename: __dirname + '/foo.js',
+      directory: __dirname
+    });
     assert(resolved.indexOf(__dirname) === 0);
   });
 
@@ -12,7 +16,11 @@ describe('resolve-dependency-path', function() {
     var depPath = './index';
     var filename = __dirname + '/foo.js';
     var directory = __dirname;
-    var resolved = resolvePath(depPath, filename, directory);
+    var resolved = resolvePath({
+      dependency: depPath,
+      filename,
+      directory
+    });
     assert(resolved === __dirname + '/index.js');
   });
 
@@ -20,7 +28,11 @@ describe('resolve-dependency-path', function() {
     var depPath = './index.js';
     var filename = __dirname + '/foo.js';
     var directory = __dirname;
-    var resolved = resolvePath(depPath, filename, directory);
+    var resolved = resolvePath({
+      dependency: depPath,
+      filename,
+      directory
+    });
     assert(resolved === __dirname + '/index.js');
   });
 
@@ -28,7 +40,11 @@ describe('resolve-dependency-path', function() {
     var depPath = 'index';
     var filename = __dirname + '/foo.js';
     var directory = __dirname;
-    var resolved = resolvePath(depPath, filename, directory);
+    var resolved = resolvePath({
+      dependency: depPath,
+      filename,
+      directory
+    });
     assert(resolved === __dirname + '/index.js');
   });
 
@@ -36,18 +52,30 @@ describe('resolve-dependency-path', function() {
     var depPath = 'index.js';
     var filename = __dirname + '/foo.js';
     var directory = __dirname;
-    var resolved = resolvePath(depPath, filename, directory);
+    var resolved = resolvePath({
+      dependency: depPath,
+      filename,
+      directory
+    });
     assert(resolved === __dirname + '/index.js');
   });
 
   it('resolves relative paths', function() {
-    var resolved = resolvePath('./bar', __dirname + '/foo.js', __dirname);
+    var resolved = resolvePath({
+      dependency: './bar',
+      filename: __dirname + '/foo.js',
+      directory: __dirname
+    });
     assert(resolved === __dirname + '/bar.js');
   });
 
   it('resolves non-relative paths', function() {
     var filename = __dirname + '/feature1/foo.js';
-    var resolved = resolvePath('feature2/bar', filename, __dirname);
+    var resolved = resolvePath({
+      dependency: 'feature2/bar',
+      filename,
+      directory: __dirname
+    });
     assert(resolved === __dirname + '/feature2/bar.js');
   });
 
@@ -59,13 +87,18 @@ describe('resolve-dependency-path', function() {
 
   it('throws if the filename is missing', function() {
     assert.throws(function() {
-      resolvePath('./bar');
+      resolvePath({
+        dependency: './bar'
+      });
     });
   });
 
   it('throws if the directory is missing', function() {
     assert.throws(function() {
-      resolvePath('./bar', __dirname + '/foo.js');
+      resolvePath({
+        dependency: './bar',
+        filename: __dirname + '/foo.js'
+      });
     });
   });
 
@@ -74,7 +107,11 @@ describe('resolve-dependency-path', function() {
       var depPath = './bar.baz.qux';
       var filename = __dirname + '/foo.js';
       var directory = __dirname;
-      var resolved = resolvePath(depPath, filename, directory);
+      var resolved = resolvePath({
+        dependency: depPath,
+        filename,
+        directory
+      });
       assert(resolved === __dirname + '/bar.baz.qux.js');
     });
 
@@ -82,7 +119,11 @@ describe('resolve-dependency-path', function() {
       var depPath = '../index.js';
       var filename = __dirname + '/foo.js';
       var directory = __dirname;
-      var resolved = resolvePath(depPath, filename, directory);
+      var resolved = resolvePath({
+        dependency: depPath,
+        filename,
+        directory
+      });
       // Extension after removing the .js extension
       var remainingExt = path.extname(path.basename(resolved, '.js'));
       assert.equal(remainingExt, '');
@@ -92,7 +133,11 @@ describe('resolve-dependency-path', function() {
       var depPath = 'styles';
       var filename = __dirname + '/foo.scss';
       var directory = __dirname;
-      var resolved = resolvePath(depPath, filename, directory);
+      var resolved = resolvePath({
+        dependency: depPath,
+        filename,
+        directory
+      });
       assert.equal(path.extname(resolved), '.scss');
     });
 
@@ -100,7 +145,11 @@ describe('resolve-dependency-path', function() {
       var depPath = 'hgn!templates/foo.mustache';
       var filename = __dirname + '/foo.js';
       var directory = __dirname;
-      var resolved = resolvePath(depPath, filename, directory);
+      var resolved = resolvePath({
+        dependency: depPath,
+        filename,
+        directory
+      });
       assert.equal(path.extname(resolved), '.mustache');
     });
   });
@@ -110,7 +159,11 @@ describe('resolve-dependency-path', function() {
       var depPath = './templates/file.css!';
       var filename = __dirname + '/foo.js';
       var directory = __dirname;
-      var resolved = resolvePath(depPath, filename, directory);
+      var resolved = resolvePath({
+        dependency: depPath,
+        filename,
+        directory
+      });
       assert.equal(path.extname(resolved), '.css');
     });
 
@@ -118,7 +171,11 @@ describe('resolve-dependency-path', function() {
       var depPath = 'templates/file.css!';
       var filename = __dirname + '/foo.js';
       var directory = __dirname;
-      var resolved = resolvePath(depPath, filename, directory);
+      var resolved = resolvePath({
+        dependency: depPath,
+        filename,
+        directory
+      });
       assert.equal(path.extname(resolved), '.css');
     });
   });
@@ -128,7 +185,11 @@ describe('resolve-dependency-path', function() {
       var depPath = './templates/file.txt!text';
       var filename = __dirname + '/foo.js';
       var directory = __dirname;
-      var resolved = resolvePath(depPath, filename, directory);
+      var resolved = resolvePath({
+        dependency: depPath,
+        filename,
+        directory
+      });
       assert.equal(path.extname(resolved), '.txt');
     });
 
@@ -136,7 +197,11 @@ describe('resolve-dependency-path', function() {
       var depPath = 'templates/file.txt!text';
       var filename = __dirname + '/foo.js';
       var directory = __dirname;
-      var resolved = resolvePath(depPath, filename, directory);
+      var resolved = resolvePath({
+        dependency: depPath,
+        filename,
+        directory
+      });
       assert.equal(path.extname(resolved), '.txt');
     });
   });
@@ -146,7 +211,11 @@ describe('resolve-dependency-path', function() {
       var depPath = './styles/foo.css';
       var filename = __dirname + '/foo.js';
       var directory = __dirname;
-      var resolved = resolvePath(depPath, filename, directory);
+      var resolved = resolvePath({
+        dependency: depPath,
+        filename,
+        directory
+      });
       assert.equal(path.extname(resolved), '.css');
     });
   });
